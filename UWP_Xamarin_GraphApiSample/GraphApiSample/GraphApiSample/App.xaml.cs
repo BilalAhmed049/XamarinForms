409d82ee78c05f9169af783173441e6349980452
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2016-11-05 11:10:40 +0100

    Microsoft Graph Api with UWP and Xamarin

---
+++ UWP_Xamarin_GraphApiSample/GraphApiSample/GraphApiSample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GraphApiSample.GraphHelpers;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Identity.Client;
+using Xamarin.Forms;
+
+namespace GraphApiSample
+{
+    public partial class App : Application
+    {
+        public static PublicClientApplication IdentityClientApp = null;
+        static string ClientID = "92b9075e-1a71-4b31-86d4-436f7cc0d75a";
+        public App()
+        {
+            IdentityClientApp = new PublicClientApplication(ClientID);
+            MainPage = new MainPage();
+        }
+
+    }
+}
