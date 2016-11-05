80108dd9ecfa037e6a130f903f1374034169d093
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2016-11-05 11:11:56 +0100

    Update App.xaml.cs

--- UWP_Xamarin_GraphApiSample/GraphApiSample/GraphApiSample/App.xaml.cs
+++ UWP_Xamarin_GraphApiSample/GraphApiSample/GraphApiSample/App.xaml.cs
@@ -12,7 +12,7 @@ namespace GraphApiSample
     public partial class App : Application
     {
         public static PublicClientApplication IdentityClientApp = null;
-        static string ClientID = "92b9075e-1a71-4b31-86d4-436f7cc0d75a";
+        static string ClientID = "<<your app id>>";
         public App()
         {
             IdentityClientApp = new PublicClientApplication(ClientID);
