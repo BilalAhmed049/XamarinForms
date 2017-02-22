d81a36cfd23422785527e9d9322c72a5aa74f795
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2017-02-22 07:25:07 +0100

    NavigationService bug resolved.

--- MvvmAppDemo/MvvmAppDemo/Infrastructure/Services/INavigationService.cs
+++ MvvmAppDemo/MvvmAppDemo/Infrastructure/Services/INavigationService.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace MvvmAppDemo
 {
 	public interface INavigationService
 	{
-		void GoBack();
+        void Configure(AppPages pageKey, Type pageType);
+        void Initialize(NavigationPage page);
+        void GoBack();
 		void NavigateTo(AppPages pageKey);
 		void NavigateTo(AppPages pageKey, object parameter);
 	}
